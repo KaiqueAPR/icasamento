c3eacf72017d29aa9db65fde346f0f1a64d9299e
Kaique Araujo <kaiquepinho2010@hotmail.com>
2023-09-26 20:32:16 -0300

    create project

---
+++ src/main/java/com/icasamento/icasamento/IcasamentoApplication.java
@@ -0,0 +1,13 @@
+package com.icasamento.icasamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcasamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcasamentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icasamento/icasamento/IcasamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icasamento.icasamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcasamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
