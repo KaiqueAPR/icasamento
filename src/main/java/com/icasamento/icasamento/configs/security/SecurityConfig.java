4b8dab110b2990b63f4efb1f81b9858957f44e38
Kaique <kaiquepinho2010@hotmail.com>
2023-11-07 11:16:49 -0300

    fix: permit login page

--- src/main/java/com/icasamento/icasamento/configs/security/SecurityConfig.java
+++ src/main/java/com/icasamento/icasamento/configs/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,10 +16,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
+                        .requestMatchers("login.html").permitAll()
                 )
                 .httpBasic(withDefaults());
         return http.build();
     }
-
 }
