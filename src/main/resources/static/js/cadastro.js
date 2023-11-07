7bd9a187e520793531e385863af08bfaa8148b2c
Kaique <kaiquepinho2010@hotmail.com>
2023-11-07 11:36:40 -0300

    fix: change fetch api

--- src/main/resources/static/js/cadastro.js
+++ src/main/resources/static/js/cadastro.js
@@ -1,14 +1,43 @@
-/* Requisição HTTP do método GRAVAR no backend
-document.getElementById("btnGravar").addEventListener("click", function () {
-    fetch('/novo', {
-        method: 'POST',
-    })
-        .then(response => response.text())
-        .then(data => {
-            console.log(data);
-            alert(data);
+document.addEventListener("DOMContentLoaded", function () {
+    const formularioCadastro = document.getElementById("formularioCadastro");
+
+    formularioCadastro.addEventListener("submit", function (event) {
+        event.preventDefault(); // Evita o comportamento padrão de envio do formulário.
+
+        // Captura os dados do formulário.
+        const txNome = document.getElementById("txNome").value;
+        const txEmail = document.getElementById("txEmail").value;
+        const nrTelefone = document.getElementById("nrTelefone").value;
+        const txCpf = document.getElementById("txCpf").value;
+        const cdSenha = document.getElementById("cdSenha").value;
+
+        // Crie um objeto com os dados para enviar como JSON.
+        const dados = {
+            txNome,
+            txEmail,
+            nrTelefone,
+            txCpf,
+            cdSenha,
+        };
+
+        // Faça a solicitação POST usando fetch.
+        fetch("/convidado/novo", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(dados),
         })
-        .catch(error => {
-            console.error('Erro:', error);
-        });
-});*/
+            .then((response) => response.json())
+            .then((data) => {
+                // Manipule a resposta do servidor, se necessário.
+                console.log(data);
+                // Você pode redirecionar o usuário para outra página, exibir uma mensagem de sucesso, etc.
+            })
+            .catch((error) => {
+                // Lidar com erros de solicitação.
+                console.error("Erro na solicitação:", error);
+                // Exibir uma mensagem de erro ou realizar outra ação apropriada.
+            });
+    });
+});
