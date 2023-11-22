8939e67883238775aea8eae1180a183094d40f99
Kaique Araujo Pinholato Ribeiro <kaiquepinho2010@hotmail.com>
2023-11-21 21:34:54 -0300

    fix: ...

--- src/main/java/com/icasamento/icasamento/repositories/cidade/CidadeRepository.java
+++ src/main/java/com/icasamento/icasamento/repositories/cidade/CidadeRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 
 @Repository
 public interface CidadeRepository extends JpaRepository<CidadeModel, Integer> {
-
     List<CidadeModel> findByNmCidadeContainingIgnoreCase(String nmCidade);
 }
